242d3abfe3874f9bb8983870f2d5ea458e8759d5
Mikhail Trunov <mikhail.trunov@gmail.com>
2021-07-08 01:39:01 +0300

    Added db file

---
+++ d.sql
@@ -0,0 +1,36 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.7
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Июл 08 2021 г., 01:37
+-- Версия сервера: 5.7.21-20-beget-5.7.21-20-1-log
+-- Версия PHP: 5.6.40
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `xatikont_django`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `d`
+--
+-- Структура чтения ошибок для таблицы xatikont_django.d: #1146 - Таблица 'xatikont_django.d' не существует
+-- Ошибка считывания данных таблицы xatikont_django.d: #1064 - У вас ошибка в запросе. Изучите документацию по используемой версии MySQL на предмет корректного синтаксиса около 'FROM `xatikont_django`.`d`' на строке 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
